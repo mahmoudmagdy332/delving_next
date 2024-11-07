004c899c0c87522b426997eda9126c749cdae7f4
mahmoud magdy <mahmoudmagdy99925332@gmail.com>
2024-11-07 17:47:36 +0200

    edits

--- src/app/[locale]/(Layout)/account/layout.tsx
+++ src/app/[locale]/(Layout)/account/layout.tsx
@@ -1,21 +1,22 @@
+import AccountSide from "@/components/Account/AccountSide";
 
-import AccountSide from "@/components/Account/AccountSide"; 
-import { useProfile } from "@/utils/hooks/useAuth"; 
-import { ReactNode } from "react";
 
-const AccountLayout = ({children}:{children:ReactNode}) => {
-  useProfile();
 
+
+ async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <div className="my-4 w-10/12 mx-auto grid  grid-cols-4">
-      <div className="col-span-3">
-        {children}
-      </div>
-      <div>
-        <AccountSide />
-      </div>
+    <div className="col-span-3">
+      {children}
     </div>
+    <div>
+      <AccountSide />
+    </div>
+  </div>
   );
-};
-
-export default AccountLayout;
+}
+export default RootLayout;
